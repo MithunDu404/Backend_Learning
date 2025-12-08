e3ee54d70de57d29b6683073a9a04ac8d473c4c3
Mithun Dutta <mithundu632@gmail.com>
2025-12-09 02:52:22 +0530

    added the successfull chapter4

--- chapter3/src/prismaClient.js
+++ chapter3/src/prismaClient.js
@@ -1,4 +1,5 @@
-import {PrismaClient} from '@prisma/client'
-const prisma = new PrismaClient();
+import { PrismaClient } from './generated/prisma/client.ts'
+
+const prisma = new PrismaClient()
 
 export default prisma;
